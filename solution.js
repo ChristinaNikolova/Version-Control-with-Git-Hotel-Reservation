08d16abb1a33ab73c744e2a7e4c5c7d9ab558964
christinanikolova <christinanikolova@github.com>
2024-09-21 15:15:09 +0300

    implement search

--- solution.js
+++ solution.js
@@ -8,6 +8,25 @@ let reservation = {
   email: null,
 };
 
+changeContent('search-form-content');
+document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
+
+function searchFormData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
+}
+
 function changeContent(className) {
   document
     .querySelectorAll(".custom-form")
@@ -24,3 +43,4 @@ document
 function cleanData(e) {
   changeContent("search-form-content");
 }
+
